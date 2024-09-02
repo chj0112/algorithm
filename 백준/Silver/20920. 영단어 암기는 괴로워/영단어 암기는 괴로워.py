fe70e15a0a2213f4eb011b2630fda36b8dd788d9
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-09-02 22:50:53 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 260 ms, Memory: 59740 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 dict = {}
 for i in range(n):
-    word = input().rstrip()
-    if len(word) >= m:
+    word = input()
+    if len(word) > m:
         if word in dict:
             dict[word] += 1
         else:
             dict[word] = 1
 for k, v in sorted(dict.items(), key= lambda x: (-x[1], -len(x[0]), x[0])):
-    sys.stdout.write(k + '\n')
+    sys.stdout.write(k)
