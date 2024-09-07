328a64124989fec42156677614281243a347ef1b
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-09-07 21:34:57 +0900

    [Silver IV] Title: 스택, Time: 488 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,23 @@
+n = int(input())
+stk = list()
+for i in range(n):
+    cmd = input().split()
+    if cmd[0] == "push":
+        stk.append(cmd[1])
+    elif cmd[0] == "pop":
+        if not stk:
+            print(-1)
+        else:
+            print(stk.pop())
+    elif cmd[0] == "size":
+        print(len(stk))
+    elif cmd[0] == "empty":
+        if not stk:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == "top":
+        if not stk:
+            print(-1)
+        else:
+            print(stk[-1])
