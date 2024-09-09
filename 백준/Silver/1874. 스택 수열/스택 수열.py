6d3a87d893fa96a4f9ceb075fe9abb8ada1c08e2
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-09-09 19:29:12 +0900

    [Silver II] Title: 스택 수열, Time: 88 ms, Memory: 37344 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,16 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 seq = [int(input()) for _ in range(n)]
 stack = []
 num = 0
-result = ""
+result = []
 for i in seq:
     while i > num:
         num += 1
         stack.append(num)
-        result += '+\n'
+        result.append('+')
     if stack.pop() == i:
-        result += '-\n'
+        result.append('-')
     else:
         print("NO")
         exit()
-print(result)
+print('\n'.join(result))
