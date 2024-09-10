0b01c3072449a522c866adef0db96b5202cc114d
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-09-11 04:50:15 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,13 @@
+def dfs(k, s, arr):
+    if k == m:
+        ans.append(arr)
+        return
+    else:
+        for i in range(s, n + 1):
+            dfs(k + 1, i + 1, arr + [i])
+
+n, m = map(int, input().split())
+ans = []
+dfs(0, 1, [])
+for lst in ans:
+    print(*lst)
