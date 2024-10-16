388560182c194d705481d32fe1d43f1e6d9b51ef
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-10-16 17:46:39 +0900

    [level 1] Title: 과일 장수, Time: 355.89 ms, Memory: 163 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Integer[] tmp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Collections.reverseOrder());
+        for (int i = m - 1; i < score.length; i += m) {
+            answer += (tmp[i] * m);
+        }
+        return answer;
+    }
+}
