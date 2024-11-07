6fec95b22df1c1f243b6392a8e54c7c6b62eed3b
Hyunjun <50340736+chj0112@users.noreply.github.com>
2024-11-07 15:10:12 +0900

    [Silver IV] Title: 가로수, Time: 2588 ms, Memory: 37096 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -4,7 +4,7 @@ n = int(input())
 tree = []
 for _ in range(n):
     tree.append(int(input()))
-d = []
+d = set()
 for i in range(1, len(tree)):
-    d.append(tree[i] - tree[i - 1])
+    d.add(tree[i] - tree[i - 1])
 print((tree[-1] - tree[0]) // math.gcd(*d) + 1 - len(tree))
